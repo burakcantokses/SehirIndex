f195c22d25fe8bd519b37dd3a60950956e08e685
Burakcan TOKSES <75482610+burakcantokses@users.noreply.github.com>
2021-01-26 17:16:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// ilk index bir bölgede kaç tane þehir?
+		// ikinci index kaç tane bölge?
+		String[][] sehirler = new String[3][3];
+		// basit matematik 3x3 = 9 tane deðiþkenimiz var.
+		sehirler[0][0] = "Ýstanbul";
+		sehirler[0][1] = "Bursa";
+		sehirler[0][2] = "Kocaeli";
+		sehirler[1][0] = "Ankara";
+		sehirler[1][1] = "Konya";
+		sehirler[1][2] = "Kayseri";
+		sehirler[2][0] = "Diyarbakýr";
+		sehirler[2][1] = "Þanlýurfa";
+		sehirler[2][2] = "Gaziantep";
+		for (int i = 0; i <=2; i++) {
+			System.out.println("======================");
+			for (int j = 0; j <= 2; j++) {
+				System.out.println(sehirler[i][j]);
+			}
+		}
+	}
+
+}
